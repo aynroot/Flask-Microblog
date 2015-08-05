1745c35f0ed7bd65ab3251404e712f9c9bfe000d
Valeria Chernenko <valeriyachernenko@gmail.com>
2015-08-06 00:04:19 +0300

    debug toolbar added

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from flask_debugtoolbar import DebugToolbarExtension
 
 
 app = Flask(__name__)
@@ -10,5 +11,9 @@ db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+toolbar = DebugToolbarExtension(app)
+toolbar.init_app(app)
 
 from app import views, models
--- config.py
+++ config.py
@@ -4,3 +4,7 @@ SECRET_KEY = '9b7f09b5-8159-4b64-8d28-5809daec63f3'
 
 # db settings
 SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/aynroot/Documents/projects/webapps/tutorials/microblog/app.db'
+
+# debug settings
+DEBUG_TB_PROFILER_ENABLED = False
+DEBUG_TB_TEMPLATE_EDITOR_ENABLED = False
