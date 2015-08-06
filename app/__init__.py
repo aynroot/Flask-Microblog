246c128303567a886d2deef3364c7bd70101d26b
Valeria Chernenko <valeriyachernenko@gmail.com>
2015-08-06 23:07:39 +0300

    400 and 500 errors pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
@@ -6,6 +9,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.debug = True
 
 db = SQLAlchemy(app)
 
@@ -15,5 +19,13 @@ login_manager.login_view = 'login'
 
 toolbar = DebugToolbarExtension(app)
 toolbar.init_app(app)
+
+if not app.debug:
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
 
 from app import views, models
--- app/views.py
+++ app/views.py
@@ -13,6 +13,17 @@ def before_request():
     flask.g.user = current_user
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+
 @app.route('/')
 @app.route('/index')
 @login_required
